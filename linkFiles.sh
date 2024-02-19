fb6c9a9a47d4282a2b3468c16b89c32ff94af9e9
Tarun Tehri <tehritarun@gmail.com>
2024-02-19 12:25:45 +0530

    added code for creating dynamic symlinks

--- linkFiles.sh
+++ linkFiles.sh
@@ -1,5 +1,21 @@
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.config/kitty/kitty.conf ~/.config//kitty/kitty.conf
-ln -s ~/dotfiles/.config/kitty/current-theme.conf ~/.config/kitty/current-theme.conf 
-ln -s ~/dotfiles/.inputrc ~/.inputrc
+#!/bin/bash
+
+contain=".git"
+desdir=$HOME
+
+# get all files and directory in current directory
+files=$(find .)
+
+for f in $files; do
+    # if it is file and not .sh file
+    if [ -f "$f" ] && [[ "$f" != *$contain* ]] && ! grep -q "$f" ignoreFiles; then
+	source=$(readlink -f "$f")
+	dest="$desdir/${f:2}"
+	dire=$(dirname "$dest")
+	# check if target directory exist if not create it
+	[ ! -d "$dire" ] && mkdir "$dire"
+	ln -s "$source" "$dest"
+	# echo "$source $dest"
+    fi
+done
+
