86a3e9c68ecf090fdee4170de5597f24ae8c0a90
Tarun Tehri <tehritarun@gmail.com>
2024-02-20 19:58:37 +0530

    added code to get user response while installation

--- completeInstall.sh
+++ completeInstall.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm alacritty kitty git starship tmux neovim
-sudo pacman -S --noconfirm tree fzf lazygit
-git clone https://github.com/tehritarun/dotfiles.git ~/dotfiles
-cd ~/dotfiles || exit
-bash linkFiles.sh
-fc-cache -fv
-git clone https://github.com/tehritarun/nvim_Config.git ~/.config/nvim
+ask(){
+    read -rp "$1 (Y/n): " resp
+    [ -z "$resp" ] || [ "$resp" = "y" ]
+}
+
+if ask "Install mendatory tools?"
+then
+    sudo pacman -S --noconfirm alacritty kitty git starship tmux neovim
+fi
+
+if ask "Install optional tools?"
+then
+    sudo pacman -S --noconfirm tree fzf lazygit
+fi
+
+if ask "Install configuration?"
+then
+    git clone https://github.com/tehritarun/dotfiles.git ~/dotfiles
+    cd ~/dotfiles || exit
+    bash linkFiles.sh
+    fc-cache -fv
+fi
+
+if ask "Install nvim configuration?"
+then
+    git clone https://github.com/tehritarun/nvim_Config.git ~/.config/nvim
+fi
--- linkFiles.sh
+++ linkFiles.sh
@@ -6,14 +6,17 @@ desdir=$HOME
 # get all files and directory in current directory
 files=$(find .)
 
+read -rp "Backup existing files (y/N): " resp
+
 for f in $files; do
     # if it is file and not .sh file
     if [ -f "$f" ] && [[ "$f" != *$contain* ]] && ! grep -q "$f" ignoreFiles; then
         source=$(readlink -f "$f")
         dest="$desdir/${f:2}"
-        if [ -f "$dest" ]
+        if [ -f "$dest" ] && [ "$resp" = "y" ]
         then
             mv "$dest" "$dest.bak"
+            # echo "backup $dest"
         fi
         dire=$(dirname "$dest")
         # check if target directory exist if not create it
