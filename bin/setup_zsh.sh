cc5ff88464b439197f243cce17e8626ed2ba22c1
Tarun Tehri <tehritarun@gmail.com>
2026-09-04 20:47:19 +0530

    progressive changes

--- bin/setup_zsh.sh
+++ bin/setup_zsh.sh
@@ -7,7 +7,7 @@ if [ "$SHELL" != "$(which zsh)" ]; then
     ./bin/install.sh zsh
     # Change default shell to zsh
     echo "Setting zsh as the default shell..."
-    sudo chsh -s "$(which zsh)"
+    chsh -s "$(which zsh)"
 else
     echo "zsh is already set as the default shell."
 fi
